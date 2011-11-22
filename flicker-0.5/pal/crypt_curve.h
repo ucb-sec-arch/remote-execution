eb4c35422ea611ebc240aee70c2d903f82563148
Hilfi Madari Alkaff <hilfialkaff@gmail.com>
2011-11-22 09:19:29 -0800

    Added asymmetric encryption/decryption functions.

---
+++ flicker-0.5/pal/crypt_curve.h
@@ -0,0 +1,12 @@
+#ifndef CURVE_DONNA_H
+#define CURVE_DONNA_H
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef int32_t s32;
+typedef int64_t limb;
+
+int curve25519_donna(u8 *mypublic, const u8 *secret, const u8 *basepoint);
+
+#endif /* CURVE_DONNA_H */
